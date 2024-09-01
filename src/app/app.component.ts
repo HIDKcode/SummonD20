102e684b5e1c6878348f1930fa5065ae06831951
HIDKcode <hidkprogramador@gmail.com>
2024-09-01 02:25:19 -0400

    01-09 Tscript menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+
+  Usuario: string = "ID-invitado";
+  Correo: string = "";
+  
+
+  constructor(private router: Router, private activatedroute: ActivatedRoute) {
+    //realizar la captura de la informacion que viene por navigationExtras
+    this.activatedroute.queryParams.subscribe(param =>{
+    //validamos si viene o no información
+    if(this.router.getCurrentNavigation()?.extras.state){
+      //capturamos informacion
+      this.Usuario = this.router.getCurrentNavigation()?.extras?.state?.['user'];
+      this.Correo = this.router.getCurrentNavigation()?.extras?.state?.['mail'];
+
+     }
+    });
+  }
+
+  ngOnInit() {
+  }
+
 }
