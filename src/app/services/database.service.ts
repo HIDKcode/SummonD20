80315c491fd56480db8354a2b3db92cae449e08e
HIDKcode <hidkprogramador@gmail.com>
2024-10-18 00:27:09 -0300

    commit 17-10 sql additions 4

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -12,6 +12,9 @@ export class DatabaseService {
 
   public database!: SQLiteObject
 
+  // Path default a imagen de perfil
+  imgPath: string './assets/images/def_profile.png';
+
   userID!: number;
   clave!: string;
   email!: string;
@@ -24,12 +27,12 @@ export class DatabaseService {
   //tablas biblioteca usuario
   t_BIBLIOTECA: string = "CREATE TABLE IF NOT EXISTS BIBLIOTECA(bibliotecaID INTEGER PRIMARY KEY AUTOINCREMENT, espacio_disponible INTEGER NOT NULL, USER_userID INTEGER,FOREIGN KEY (USER_userID) REFERENCES USER(userID));";
   t_CARPETA: string = "CREATE TABLE IF NOT EXISTS CARPETA(carpetaID INTEGER PRIMARY KEY AUTOINCREMENT,parent_carpetaID INTEGER,nombre TEXT NOT NULL,creacion_date TEXT NOT NULL, BIBLIOTECA_bibliotecaID INTEGER,parent_BIBLIOTECA_bibliotecaID INTEGER,FOREIGN KEY (BIBLIOTECA_bibliotecaID) REFERENCES BIBLIOTECA(bibliotecaID),FOREIGN KEY (parent_BIBLIOTECA_bibliotecaID) REFERENCES BIBLIOTECA(bibliotecaID));";
-  t_ARCHIVO: string = "CREATE TABLE IF NOT EXISTS t();"; 
+  t_ARCHIVO: string = "CREATE TABLE IF NOT EXISTS ARCHIVO(archivoID INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT NOT NULL,tamaño INTEGER NOT NULL,filepath_arc TEXT NOT NULL,tipo_archivo TEXT NOT NULL,subida_date TEXT NOT NULL,CARPETA_carpetaID INTEGER,CARPETA_BIBLIOTECA_bibliotecaID INTEGER,FOREIGN KEY (CARPETA_carpetaID) REFERENCES CARPETA(carpetaID),FOREIGN KEY (CARPETA_BIBLIOTECA_bibliotecaID) REFERENCES BIBLIOTECA(bibliotecaID));"; 
   //tablas sistema de salas
-  t_GRUPO: string = "CREATE TABLE IF NOT EXISTS t();";
-  t_PARTICIPANTE: string = "CREATE TABLE IF NOT EXISTS t();";
-  t_MENSAJE: string = "CREATE TABLE IF NOT EXISTS t();";
-  t_ADJUNTO: string = "CREATE TABLE IF NOT EXISTS t();";
+  t_GRUPO: string = "CREATE TABLE IF NOT EXISTS GRUPO(grupoID INTEGER PRIMARY KEY AUTOINCREMENT,clave TEXT NOT NULL,descripcion TEXT,fechacreado TEXT NOT NULL);";
+  t_PARTICIPANTE: string = "CREATE TABLE IF NOT EXISTS PARTICIPANTE(participanteID INTEGER PRIMARY KEY AUTOINCREMENT,USER_userID INTEGER,GRUPO_grupoID INTEGER,FOREIGN KEY (USER_userID) REFERENCES USER(userID),FOREIGN KEY (GRUPO_grupoID) REFERENCES GRUPO(grupoID));";
+  t_MENSAJE: string = "CREATE TABLE IF NOT EXISTS MENSAJE(msjID INTEGER PRIMARY KEY AUTOINCREMENT,msj_autor TEXT NOT NULL,msj_texto TEXT NOT NULL,msj_media TEXT,msj_date TEXT NOT NULL,PARTICIPANTE_participanteID INTEGER,FOREIGN KEY (PARTICIPANTE_participanteID) REFERENCES PARTICIPANTE(participanteID));";
+  t_ADJUNTO: string = "CREATE TABLE IF NOT EXISTS ADJUNTO(mediaID INTEGER PRIMARY KEY AUTOINCREMENT,enviado_date TEXT NOT NULL,  -- Use TEXT for ISO8601 date formatfilepath_msj TEXT NOT NULL,media_tipo TEXT,MENSAJE_msjID INTEGER,FOREIGN KEY (MENSAJE_msjID) REFERENCES MENSAJE(msjID));";
 
   //insert por defecto en la Base de Datos
   r_user: string = " ";
