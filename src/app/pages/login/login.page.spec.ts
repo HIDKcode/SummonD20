5ee7cdb4016409c7ed9b04b954e959079bcab710
Matias <168784372+matipley01000@users.noreply.github.com>
2024-11-19 23:10:14 -0300

    Add files via upload

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,11 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPage } from './login.page';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { of } from 'rxjs';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { DatabaseService } from 'src/app/services/database.service';
+
+const mockDB = {
+  fetchProductos: () => of([]),
+  dbState: () => of([])
+};
+
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
-  beforeEach(() => {
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginPage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: DatabaseService, useValue: mockDB}, {provide:NativeStorage}
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
