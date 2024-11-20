9b79a631887495845f0e6b5bb9d9fa940603edb2
Matias <168784372+matipley01000@users.noreply.github.com>
2024-11-19 23:06:17 -0300

    Add files via upload

--- src/app/pages/biblioteca/biblioteca.page.spec.ts
+++ src/app/pages/biblioteca/biblioteca.page.spec.ts
@@ -1,11 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BibliotecaPage } from './biblioteca.page';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { of } from 'rxjs';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { DatabaseService } from 'src/app/services/database.service';
+
+const mockDB = {
+  fetchProductos: () => of([]),
+  dbState: () => of([])
+};
+
 describe('BibliotecaPage', () => {
   let component: BibliotecaPage;
   let fixture: ComponentFixture<BibliotecaPage>;
 
-  beforeEach(() => {
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [BibliotecaPage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: DatabaseService, useValue: mockDB}, {provide:NativeStorage}
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(BibliotecaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
