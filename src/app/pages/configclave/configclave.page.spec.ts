e4d8448a2fe26690dce6724340dcc10ee3dc3373
Matias <168784372+matipley01000@users.noreply.github.com>
2024-11-19 23:08:45 -0300

    Add files via upload

--- src/app/pages/configclave/configclave.page.spec.ts
+++ src/app/pages/configclave/configclave.page.spec.ts
@@ -1,11 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ConfigclavePage } from './configclave.page';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { of } from 'rxjs';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { DatabaseService } from 'src/app/services/database.service';
+
+const mockDB = {
+  fetchProductos: () => of([]),
+  dbState: () => of([])
+};
+
 describe('ConfigclavePage', () => {
   let component: ConfigclavePage;
   let fixture: ComponentFixture<ConfigclavePage>;
 
-  beforeEach(() => {
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [ConfigclavePage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: DatabaseService, useValue: mockDB}, {provide:NativeStorage}
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ConfigclavePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
