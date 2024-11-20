3076ee76719cfc81d81aeca2c781cd8151c5efe4
Matias <168784372+matipley01000@users.noreply.github.com>
2024-11-19 23:12:18 -0300

    Add files via upload

--- src/app/pages/rolldice/rolldice.page.spec.ts
+++ src/app/pages/rolldice/rolldice.page.spec.ts
@@ -1,11 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RolldicePage } from './rolldice.page';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { of } from 'rxjs';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { DatabaseService } from 'src/app/services/database.service';
+import { HttpClientTestingModule} from '@angular/common/http/testing';
+
 describe('RolldicePage', () => {
   let component: RolldicePage;
   let fixture: ComponentFixture<RolldicePage>;
 
-  beforeEach(() => {
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [RolldicePage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        {provide:HttpClient},{provide:NativeStorage}
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RolldicePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
